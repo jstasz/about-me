085a0012a963a3e94abdcbbd9b29357ba44c8f6f
Justyna <jszajuk@gmail.com>
2022-08-18 21:15:14 +0200

    add leaflet map and create css styles for add new restaurant section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 "use strict";
+
+import { leaflet } from "leaflet/dist/leaflet.js";
+
+if (navigator.geolocation)
+	navigator.geolocation.getCurrentPosition(
+		function (position) {
+			const { latitude } = position.coords;
+			const { longitude } = position.coords;
+
+			const map = L.map("map").setView([51.505, -0.09], 13);
+
+			L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+				attribution:
+					'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+			}).addTo(map);
+
+			L.marker([51.5, -0.09])
+				.addTo(map)
+				.bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+				.openPopup();
+		},
+
+		function () {
+			alert("could not get your position");
+		}
+	);

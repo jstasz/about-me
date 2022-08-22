6c77285e6857a74137f7091ab6cc8664292561dc
Justyna <jszajuk@gmail.com>
2022-08-22 14:42:10 +0200

    layout and initial js for scroll to restaurants list after add or find place

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 # restaurants-app
 
-To do: 
-html and css 
-add map 
+To do:
+html and css
+add map
 render place
 list of place
 add mark
 sort results
 local storage
-
-
-
--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+//map
+
 import { leaflet } from "leaflet/dist/leaflet.js";
 
 if (navigator.geolocation)
@@ -25,3 +27,66 @@ if (navigator.geolocation)
 			alert("could not get your position");
 		}
 	);
+
+// active add and find restaurant
+
+const addBtn = document.querySelector(".main__icon--add");
+const mapBox = document.querySelector(".main__map");
+const addBox = document.querySelector(".main__add-restaurant");
+const addForm = document.querySelector(".restaurant__form--add");
+const findBtn = document.querySelector(".main__icon--find");
+const findForm = document.querySelector(".restaurant__form--find");
+const icons = document.querySelector(".main__icons");
+const addSubmitBtn = document.querySelector(".form__btn--add");
+const findSubmitBtn = document.querySelector(".form__btn--find");
+const restaurantlistSection = document.querySelector(".restaurants");
+
+const firstLayout = function () {
+	mapBox.style.height = "65%";
+	addBox.style.height = "15%";
+};
+
+const changeLayout = function () {
+	mapBox.style.height = "40%";
+	addBox.style.height = "40%";
+};
+
+const showAddForm = function () {
+	addForm.classList.remove("form--hidden");
+	icons.classList.add("hidden");
+};
+
+addBtn.addEventListener("click", changeLayout);
+addBtn.addEventListener("click", showAddForm);
+
+const showFindForm = function () {
+	findForm.classList.remove("form--hidden");
+	icons.classList.add("hidden");
+};
+
+const changeLayout1 = function () {
+	mapBox.style.height = "50%";
+	addBox.style.height = "30%";
+};
+
+findBtn.addEventListener("click", changeLayout1);
+findBtn.addEventListener("click", showFindForm);
+
+const deleteAddForm = function () {
+	icons.classList.remove("hidden");
+	findForm.classList.add("form--hidden");
+	addForm.classList.add("form--hidden");
+};
+
+const scrolltoList = function (e) {
+	e.preventDefault();
+	const listTop = restaurantlistSection.offsetTop;
+	window.scrollTo(0, listTop - 10);
+	deleteAddForm();
+	firstLayout();
+};
+
+// scrolltoList();
+
+addSubmitBtn.addEventListener("click", scrolltoList);
+findSubmitBtn.addEventListener("click", scrolltoList);

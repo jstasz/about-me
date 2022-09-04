63e06132e5e5692c8adf489014cafc45e7cbca7b
Justyna <jszajuk@gmail.com>
2022-09-04 21:20:10 +0200

    cleaning the code - js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,30 @@
 "use strict";
 
-const addIcon = document.querySelector(".main__forms-icon--add");
 const mapBox = document.querySelector(".main__map");
-const addBox = document.querySelector(".main__forms");
+
+const formsBox = document.querySelector(".main__forms");
+const formsBoxContent = document.querySelector(".main__forms-box");
+
 const addForm = document.querySelector(".main__forms--add");
-const closeFormIcon = document.querySelectorAll(".form__close");
-const findIcon = document.querySelector(".activator__form--find");
 const findForm = document.querySelector(".main__forms--find");
-const formsBox = document.querySelector(".main__forms-box");
-const addSubmitBtn = document.querySelector(".form__btn--add");
+
+const findActivator = document.querySelector(".activator__form--find");
 const findSubmitBtn = document.querySelector(".form__btn--find");
-const restaurantsSection = document.querySelector(".restaurants");
+const closeFormIcon = document.querySelectorAll(".form__close");
+
 const inputName = addForm.querySelector(".form__input--name");
 const inputFood = addForm.querySelector(".form__input--food");
 const inputService = addForm.querySelector(".form__input--service");
 const inputPrice = addForm.querySelector(".form__input--price");
 const inputImpress = addForm.querySelector(".form__input--impress");
+
+const restaurantsSection = document.querySelector(".restaurants");
+
 const restaurantList = document.querySelector(".restaurants__all");
 const restaurantActiveList = document.querySelector(".restaurants__active");
 const restaurantListEl = restaurantList.querySelectorAll(".restaurant");
-const markers = document.querySelectorAll(".leaflet-marker-icon");
+
+const pages = document.querySelector(".restaurants__pages");
 const nextPage = document.querySelector(".restaurants__pages-change--next");
 const previousPage = document.querySelector(
 	".restaurants__pages-change--previous"
@@ -59,65 +64,16 @@ class App {
 	constructor() {
 		this._getLocalStorage();
 		this._getPosition();
+		this._generatePagesMarkup();
 		addForm.addEventListener("submit", this._newRestaurant.bind(this));
-		restaurantList.addEventListener("click", this._moveToPopup.bind(this));
+		findForm.addEventListener("submit", this._findRestaurant.bind(this));
+		findActivator.addEventListener("click", this._showFindForm.bind(this));
 		closeFormIcon.forEach((icon) =>
 			icon.addEventListener("click", this._startedLayout.bind(this))
 		);
-		findIcon.addEventListener("click", this._showFindForm.bind(this));
-		findSubmitBtn.addEventListener("click", this._findRestaurant.bind(this));
+		restaurantList.addEventListener("click", this._moveToPopup.bind(this));
 		nextPage.addEventListener("click", this._changePage.bind(this));
 		previousPage.addEventListener("click", this._changePage.bind(this));
-		this._generatePagesMarkup();
-	}
-
-	_renderRestaurantsPage(page) {
-		const renderRestaurants = this._divideSerchResults(page);
-
-		const restaurantListEl = restaurantList.querySelectorAll(".restaurant");
-		restaurantListEl.forEach((el) => el.remove());
-
-		renderRestaurants.forEach((restaurant) =>
-			this._renderRestaurant(restaurant, restaurantList)
-		);
-	}
-
-	_changePage(e) {
-		const side = e.target.closest(".restaurants__pages-change");
-
-		if (side === nextPage) this.#page++;
-		if (side === previousPage) this.#page--;
-
-		this._renderRestaurantsPage(this.#page);
-
-		this._generatePagesMarkup();
-	}
-
-	_generatePagesMarkup() {
-		const numPages = Math.ceil(this.#restaurants.length / 5);
-
-		if (this.#page === 1 && numPages > 1) {
-			nextPage.classList.remove("hidden");
-			previousPage.classList.add("hidden");
-		}
-
-		if (this.#page === numPages && numPages > 1) {
-			nextPage.classList.add("hidden");
-			previousPage.classList.remove("hidden");
-		}
-
-		if (this.#page < numPages && this.#page > 1) {
-			nextPage.classList.remove("hidden");
-			previousPage.classList.remove("hidden");
-		}
-
-		if (this.#page === 1 && numPages === 1) {
-			nextPage.classList.add("hidden");
-			previousPage.classList.add("hidden");
-		}
-
-		const pageNumber = document.querySelector(".restaurants__pages-number");
-		pageNumber.textContent = this.#page;
 	}
 
 	_getPosition() {
@@ -148,50 +104,6 @@ class App {
 		this.#restaurants.forEach((restaurant) =>
 			this._renderRestaurantMarker(restaurant)
 		);
-	}
-
-	_showAddForm(mapE) {
-		if (!findForm.classList.contains("form--hidden")) return;
-
-		this.#mapEvent = mapE;
-		addForm.classList.remove("form--hidden");
-		this._changeLayout();
-	}
-
-	_showFindForm() {
-		findForm.classList.remove("form--hidden");
-		this._changeLayout();
-	}
-
-	_changeLayout() {
-		mapBox.style.height = "45%";
-		addBox.style.height = "35%";
-		formsBox.classList.add("hidden");
-		inputName.focus();
-	}
-
-	_startedLayout() {
-		this._clearForms(addForm);
-		this._clearForms(findForm);
-		mapBox.style.height = "65%";
-		addBox.style.height = "15%";
-	}
-
-	_clearForms(form) {
-		formsBox.classList.remove("hidden");
-		form.classList.add("form--hidden");
-		form.classList.add("form--hidden");
-		form.querySelector(".form__input--name").value = "";
-
-		if (form === addForm) {
-			//prettier-ignore
-			form.querySelector(".form__input--food").value = form.querySelector(".form__input--service").value = form.querySelector(".form__input--price").value = form.querySelector(".form__input--impress").value = "1"
-		}
-	}
-
-	_scrolltoList(e) {
-		const listTop = restaurantsSection.offsetTop;
-		window.scrollTo(0, listTop - 10);
 	}
 
 	_newRestaurant(e) {
@@ -236,6 +148,50 @@ class App {
 		setTimeout(this._scrolltoList, 1500);
 	}
 
+	_renderRestaurant(restaurant, list) {
+		let html = `
+		<li class="restaurant restaurant-${
+			restaurant.average >= 6 ? "heighscore" : "lowscore"
+		}" data-id="${restaurant.id}" data-rname="${restaurant.name}" >
+			<div class="restaurant__header">
+				<h3 class="restaurant__header-title">${restaurant.name}</h3>
+				<p class="restaurant__header-average">${restaurant.average >= 7 ? "" : ""} ${
+			restaurant.average
+		}</p>
+			</div>
+			<div class="restaurant__scoores">
+				<p class="restaurant__icon--eat">🍔 = ${restaurant.food}</p>
+				<p class="restaurant__icon--service">🍽 = ${restaurant.service}</p>
+				<p class="restaurant__icon--price">💸 = ${restaurant.price}</p>
+				<p class="restaurant__icon--impress">❤️ = ${restaurant.impress}</p>
+			</div>
+		</li>
+		`;
+
+		list.insertAdjacentHTML("beforeend", html);
+
+		if (list === restaurantActiveList)
+			restaurantActiveList.classList.remove("hidden");
+	}
+
+	_renderRestaurantsPage(page) {
+		const renderRestaurants = this._divideSerchResults(page);
+
+		const restaurantListEl = restaurantList.querySelectorAll(".restaurant");
+		restaurantListEl.forEach((el) => el.remove());
+
+		renderRestaurants.forEach((restaurant) =>
+			this._renderRestaurant(restaurant, restaurantList)
+		);
+	}
+
+	_divideSerchResults(page) {
+		const start = (page - 1) * 5; //0;
+		const end = page * 5; // 5
+
+		return this.#restaurants.slice(start, end);
+	}
+
 	_renderRestaurantMarker(restaurant) {
 		this.#marker = L.marker(restaurant.coords)
 			.addTo(this.#map)
@@ -271,32 +227,6 @@ class App {
 			.openPopup();
 	}
 
-	_renderRestaurant(restaurant, list) {
-		let html = `
-		<li class="restaurant restaurant-${
-			restaurant.average >= 6 ? "heighscore" : "lowscore"
-		}" data-id="${restaurant.id}" data-rname="${restaurant.name}" >
-			<div class="restaurant__header">
-				<h3 class="restaurant__header-title">${restaurant.name}</h3>
-				<p class="restaurant__header-average">${restaurant.average >= 7 ? "" : ""} ${
-			restaurant.average
-		}</p>
-			</div>
-			<div class="restaurant__scoores">
-				<p class="restaurant__icon--eat">🍔 = ${restaurant.food}</p>
-				<p class="restaurant__icon--service">🍽 = ${restaurant.service}</p>
-				<p class="restaurant__icon--price">💸 = ${restaurant.price}</p>
-				<p class="restaurant__icon--impress">❤️ = ${restaurant.impress}</p>
-			</div>
-		</li>
-		`;
-
-		list.insertAdjacentHTML("beforeend", html);
-
-		if (list === restaurantActiveList)
-			restaurantActiveList.classList.remove("hidden");
-	}
-
 	_moveToPopup(e) {
 		const restaurantListEl = e.target.closest(".restaurant");
 
@@ -323,7 +253,7 @@ class App {
 		const restaurantName = findForm.querySelector(".form__input--name").value;
 
 		const restaurant = this.#restaurants.find(
-			(rest) => rest.name === restaurantName
+			(rest) => rest.name.toUpperCase() === restaurantName.toUpperCase()
 		);
 
 		if (!restaurant) {
@@ -359,17 +289,101 @@ class App {
 		setTimeout(this._scrolltoList, 2000);
 	}
 
+	_showAddForm(mapE) {
+		if (!findForm.classList.contains("form--hidden")) return;
+
+		this.#mapEvent = mapE;
+		addForm.classList.remove("form--hidden");
+		this._changeLayout();
+	}
+
+	_showFindForm() {
+		findForm.classList.remove("form--hidden");
+		this._changeLayout();
+	}
+
+	_changeLayout() {
+		mapBox.style.height = "45%";
+		formsBox.style.height = "35%";
+		formsBoxContent.classList.add("hidden");
+		inputName.focus();
+	}
+
+	_startedLayout() {
+		this._clearForms(addForm);
+		this._clearForms(findForm);
+		mapBox.style.height = "65%";
+		formsBox.style.height = "15%";
+	}
+
+	_clearForms(form) {
+		formsBoxContent.classList.remove("hidden");
+		form.classList.add("form--hidden");
+		form.classList.add("form--hidden");
+		form.querySelector(".form__input--name").value = "";
+
+		if (form === addForm) {
+			//prettier-ignore
+			form.querySelector(".form__input--food").value = form.querySelector(".form__input--service").value = form.querySelector(".form__input--price").value = form.querySelector(".form__input--impress").value = "1"
+		}
+	}
+
 	_clearActiveRestaurants() {
 		const restaurants = restaurantList.querySelectorAll(".restaurant");
 
 		restaurants.forEach((restaurant) =>
 			restaurant.classList.remove("restaurant--active")
 		);
-
 		const activeRestaurants =
 			restaurantActiveList.querySelectorAll(".restaurant");
 
 		activeRestaurants.forEach((el) => el.remove());
+	}
+
+	_changePage(e) {
+		const side = e.target.closest(".restaurants__pages-change");
+
+		if (side === nextPage) this.#page++;
+		if (side === previousPage) this.#page--;
+
+		this._renderRestaurantsPage(this.#page);
+
+		if (restaurantList.childElementCount === 0) {
+			pages.style.display = "none";
+		}
+		this._generatePagesMarkup();
+	}
+
+	_generatePagesMarkup() {
+		const numPages = Math.ceil(this.#restaurants.length / 5);
+
+		if (this.#page === 1 && numPages > 1) {
+			nextPage.classList.remove("hidden");
+			previousPage.classList.add("hidden");
+		}
+
+		if (this.#page === numPages && numPages > 1) {
+			nextPage.classList.add("hidden");
+			previousPage.classList.remove("hidden");
+		}
+
+		if (this.#page < numPages && this.#page > 1) {
+			nextPage.classList.remove("hidden");
+			previousPage.classList.remove("hidden");
+		}
+
+		if (this.#page === 1 && numPages === 1) {
+			nextPage.classList.add("hidden");
+			previousPage.classList.add("hidden");
+		}
+
+		const pageNumber = document.querySelector(".restaurants__pages-number");
+		pageNumber.textContent = this.#page;
+	}
+
+	_scrolltoList(e) {
+		const listTop = restaurantsSection.offsetTop;
+		window.scrollTo(0, listTop - 10);
 	}
 
 	_setLocalStorage() {
@@ -391,13 +405,6 @@ class App {
 	reset() {
 		localStorage.removeItem("restaurants");
 		location.reload();
-	}
-
-	_divideSerchResults(page) {
-		const start = (page - 1) * 5; //0;
-		const end = page * 5; // 5
-
-		return this.#restaurants.slice(start, end);
 	}
 }
 
